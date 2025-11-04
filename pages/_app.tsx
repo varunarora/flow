de5b683536f6b9af04ff6be6452dabfd8ab6fd54
Varun Arora <varun@opencurriculum.org>
2025-11-04 10:38:44 -0800

    Use appropriate auth domain based on environment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ type AppPropsWithLayout = AppProps & {
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     const firebaseConfig = {
         apiKey: process.env.NEXT_PUBLIC_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+        authDomain: process.env.NODE_ENV === 'development' ? 'localhost:3000' : process.env.NEXT_PUBLIC_AUTH_DOMAIN,
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
         storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
         messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
